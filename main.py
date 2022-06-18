95147796295c7617ca27e5e60e41dfba6f7a9db5
Noxxxxious <michal.wrzosek.robert@gmail.com>
2022-06-18 07:25:19 +0200

    done

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
 import pygame
+import math
+import time
+import random
 
 SCREEN_H = 600
 SCREEN_W = 800
 pixel_color = (255, 255, 255)
 center_x = int(SCREEN_W / 2)
 center_y = int(SCREEN_H / 2)
-
 icon = pygame.image.load("icon.png")
-pygame.display.set_icon(icon)
-surface = pygame.display.set_mode((SCREEN_W, SCREEN_H))
-pygame.display.set_caption("Sierpinski Fractals")
+caption = "Fractals"
 
-while True:
-    surface.set_at((center_x, center_y), pixel_color)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-    pygame.display.update()
+# modify this value to generate a different fractal
+N = 10
+PI = math.pi
+R = 200
+vertices = [(math.cos(2 * PI / N * x) * R + center_x, math.sin(2 * PI / N * x) * R + center_y) for x in range(0, N + 1)]
+
+
+def two_random_adjacent_vertices():
+    index = random.randint(0, N - 1)
+    index2 = (index + 1) % N if random.randint(0, 1) else (index - 1) % N
+    ver1 = vertices[index]
+    ver2 = vertices[index2]
+    return ver1, ver2
+
+
+def main():
+    pygame.display.set_icon(icon)
+    surface = pygame.display.set_mode((SCREEN_W, SCREEN_H))
+    pygame.display.set_caption(caption)
+    prev_point = (center_x, center_y)
+    while True:
+        (v1, v2) = two_random_adjacent_vertices()
+        new_point_x = round((v1[0] + v2[0] + prev_point[0]) / 3)
+        new_point_y = round((v1[1] + v2[1] + prev_point[1]) / 3)
+        surface.set_at((new_point_x, new_point_y), pixel_color)
+        prev_point = (new_point_x, new_point_y)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+        pygame.display.update()
+
+
+main()
